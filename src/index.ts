2cdbb9e180f7879d07533a4cebbeeb4bbf446496
John Wright <johngeorge.wright@gmail.com>
2019-02-09 23:20:43 +0000

    fix: 🐛 add building script

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
 export { default as accumulator } from './accumulator'
 export { default as reducer } from './reducer'
-export { Middleware, Next, State } from './types'
+export { Middleware, Next } from './types'
