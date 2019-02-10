c5e44c1007fbc3a115d650094bc5d6281a320313
John Wright <johngeorge.wright@gmail.com>
2019-02-10 14:56:36 +0000

    refactor: 💡 simplify with async/await

--- src/accumulator.ts
+++ src/accumulator.ts
@@ -1,4 +1,4 @@
-import { Middleware, Next } from './types'
+import { Middleware } from './types'
 
 export interface State {
   [key: string]: any
@@ -10,22 +10,17 @@ export default (middleware: Middleware<State>[] = [], middlewareArgs: any[] = []
 
     const dispatch = async (i: number, nextState: State = {}): Promise<State> => {
       if (i <= index) {
-        return Promise.reject(new Error('next() called multiple times'))
+        throw new Error('next() called multiple times')
       }
 
       index = i
       state = { ...state, ...nextState }
 
       if (i === middleware.length) {
-        return Promise.resolve(state)
+        return state
       }
 
-      try {
-        const result = middleware[i](state, dispatch.bind(null, i + 1), ...middlewareArgs)
-        return Promise.resolve(result)
-      } catch (err) {
-        return Promise.reject(err)
-      }
+      return middleware[i](state, dispatch.bind(null, i + 1), ...middlewareArgs)
     }
 
     return dispatch(0, state)
